bd48dace12835ef3f40cb79432c5c16b9a0b2b83
YihuiAndre <47070337+YihuiAndre@users.noreply.github.com>
2020-04-07 22:32:17 -0400

    Revert "Revert "Revert "Exercise 1, Composition of Shape and Shape Overlapping Testing"""
    This reverts commit 8ae45f48c34c1f510408d074f1ac8c04321d6d0b.

--- Exercise 2/MyShapePosition.java
+++
@@ -1,8 +0,0 @@
-
-
-public interface MyShapePosition extends MyPoint{
-	//return the bounding box of an object
-	MyRectangle getMyBoundingBox();
-	//return true if two object is overlapping
-	boolean doOverlap(MyShape p);
-}
